b06cf0d9d6892e87531deddca271f0d9625c2e5e
mamaquig <marvyndany@gmail.com>
2023-03-09 03:04:11 +0100

    ex10 done

--- ex10/loading.py
+++ ex10/loading.py
@@ -1,4 +1,3 @@
-import sys
 import time
 
 
@@ -7,18 +6,21 @@ def ft_progress(listy):
     max_cycle = len(listy)
     start_cycle = time.monotonic()
     for elem in listy:
-        time_cycle = (time.monotonic() - start_cycle) * (max_cycle - elem+1)
+        time_loop = (time.monotonic() - start_cycle) * (max_cycle-1 - elem)
+        pourcent = int(((elem+1)/max_cycle)*30)
         print(end="\x1b[2K")
-        print(f"ETA: {time_cycle:.2f}s [{int(((elem+1) / max_cycle) * 100):3d}%][{}>  ] {elem+1}/{max_cycle} | elapsed time {time.monotonic() - start:.2f}s", flush=True, end="\r")
+        print(f"ETA: {time_loop:.2f}s [{int(((elem+1) / max_cycle) * 100):3d} \
+%][{pourcent * '='}>{(30 - pourcent) * ' '}] {elem+1}/{max_cycle} \
+| elapsed time {time.monotonic() - start:.2f}s", flush=True, end="\r")
         start_cycle = time.monotonic()
         yield elem
     print()
 
 
-listy = range(300)
+listy = range(3333)
 ret = 0
 for elem in ft_progress(listy):
-    ret += (elem + 3) % 5
-    time.sleep(0.01)
+    ret += elem
+    time.sleep(0.005)
 print()
 print(ret)
